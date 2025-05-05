10cbb128fff1417c0275dd0583b599426cb61131
Mohamad Zangane <mohamad.zangane@ymail.com>
2025-05-05 12:04:51 +0330

    Create stopwords.php

---
+++ stopwords.php
@@ -0,0 +1,8 @@
+<?php
+
+$stopWords = [
+    "the", "is", "in", "at", "of", "a", "and", "to", "on", "for", "by", "with",
+    "an", "be", "as", "it", "this", "that", "from", "or", "are", "was", "were",
+    "has", "had", "have", "but", "not", "they", "you", "we", "he", "she", "him",
+    "her", "them", "his", "its", "our", "their", "my", "your"
+];
